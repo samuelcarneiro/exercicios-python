cf4aa209760e4a2b77fbabe1432bc92f2e07a7b5
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2025-12-16 00:25:54 -0300

    Exercício 002 - Mensagem boas-vindas

---
+++ ex002.py
@@ -0,0 +1,9 @@
+# Exercício 002
+# Faça um programa que leia o nome de uma pessoa e mostre
+# uma mensagem de boas-vindas
+
+nome = input('Informe seu nome: ')
+
+mensagem = 'Olá {}, seja bem-vindo!'.format(nome)
+
+print(mensagem)
